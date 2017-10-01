92a550fbc895672b6399a064fe2407687ce93e38
Neeraj Jaiswal <neerajj85@gmail.com>
2017-10-01 15:48:32 +0530

    clone link updated

--- tools/install.sh
+++ tools/install.sh
@@ -59,7 +59,7 @@ main() {
       exit 1
     fi
   fi
-  env git clone --depth=1 https://github.com/nrjais/oh-my-zsh.git -b develop $ZSH || {
+  env git clone --depth=1 https://github.com/nrjais/oh-my-zsh.git -b master $ZSH || {
     printf "Error: git clone of oh-my-zsh repo failed\n"
     exit 1
   }
